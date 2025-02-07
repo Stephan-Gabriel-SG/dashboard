6d085adfe67c0d298d20ae4fdb5c999f8c2a62b3
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-02-07 18:47:04 +0300

    update my site

--- js.js
+++ js.js
@@ -6,4 +6,21 @@ function show(event) {
     } else {
         document.getElementById('input-search').focus();
     }
-}    
+}
+
+function showAlert(opt) {
+    const author = "NANDRASANTSOA Stéphan Gabriel";
+    
+    const messages = [
+        `🚀 Awesome choice! Option ${opt} has been selected!`,
+        `🎉 Nice one! You just clicked on option ${opt}!`,
+        `🔥 Option ${opt} activated! Ready for an adventure?`,
+        `😎 Boom! Option ${opt} is now in action! Let's go!`,
+        `🛠️ Done! Option ${opt} is now being used!`
+    ];
+    
+    // Pick a random message and add your name
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)] + `\n\n- Author : ${author}`;
+
+    alert(randomMessage);
+}
